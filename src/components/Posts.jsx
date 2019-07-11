7b1b68829cc95fa09f485b889a8de9ffdaa25ce1
Jason Watt <jason.watt@ie.com.au>
2019-07-11 12:51:00 +1000

    Initial Commit

---
+++ src/components/Posts.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export default class Posts extends Component {
+  render() {
+    return (
+      <ul>
+        {this.props.posts.map((post, i) => (
+          <li key={i}>
+            <a href={post.url} target="_blank" rel="noopener noreferrer">
+              {post.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+Posts.propTypes = {
+  posts: PropTypes.array.isRequired
+};
